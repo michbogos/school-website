24cd03dc035c832200ff71600eee1e0a023c23bf
Michael Bogosta <bogostamicheal@gmail.com>
2022-11-06 11:45:59 +0100

    Added dual layout

--- src/Org.js
+++ src/Org.js
@@ -7,12 +7,21 @@ export default function Org() {
       <TopBar></TopBar>
       <div className='background' id="org"></div>
       <div className='stack center'>
-      <div className='card'>
-        <h1>Vario</h1>
-        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque a augue magna. Maecenas vitae tellus sed ligula viverra pulvinar. Donec in justo sagittis magna egestas auctor in eu erat. Fusce eu lacinia metus, id ultricies leo. Integer tempus, ante at mattis suscipit, arcu dolor venenatis mauris, molestie consequat erat mi. </p>
-        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque a augue magna. Maecenas vitae tellus sed ligula viverra pulvinar. Donec in justo sagittis magna egestas auctor in eu erat. Fusce eu lacinia metus, id ultricies leo. Integer tempus, ante at mattis suscipit, arcu dolor venenatis mauris, molestie consequat erat mi. </p>
-        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque a augue magna. Maecenas vitae tellus sed ligula viverra pulvinar. Donec in justo sagittis magna egestas auctor in eu erat. Fusce eu lacinia metus, id ultricies leo. Integer tempus, ante at mattis suscipit, arcu dolor venenatis mauris, molestie consequat erat mi. </p>
-      </div>
+        <div className='row'>
+          <div className='card'>
+            <h1>Vario</h1>
+            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque a augue magna. Maecenas vitae tellus sed ligula viverra pulvinar. Donec in justo sagittis magna egestas auctor in eu erat. Fusce eu lacinia metus, id ultricies leo. Integer tempus, ante at mattis suscipit, arcu dolor venenatis mauris, molestie consequat erat mi. </p>
+            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque a augue magna. Maecenas vitae tellus sed ligula viverra pulvinar. Donec in justo sagittis magna egestas auctor in eu erat. Fusce eu lacinia metus, id ultricies leo. Integer tempus, ante at mattis suscipit, arcu dolor venenatis mauris, molestie consequat erat mi. </p>
+            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque a augue magna. Maecenas vitae tellus sed ligula viverra pulvinar. Donec in justo sagittis magna egestas auctor in eu erat. Fusce eu lacinia metus, id ultricies leo. Integer tempus, ante at mattis suscipit, arcu dolor venenatis mauris, molestie consequat erat mi. </p>
+          </div>
+          <div className='card'>
+            <h1>Vario</h1>
+            
+            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque a augue magna. Maecenas vitae tellus sed ligula viverra pulvinar. Donec in justo sagittis magna egestas auctor in eu erat. Fusce eu lacinia metus, id ultricies leo. Integer tempus, ante at mattis suscipit, arcu dolor venenatis mauris, molestie consequat erat mi. </p>
+            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque a augue magna. Maecenas vitae tellus sed ligula viverra pulvinar. Donec in justo sagittis magna egestas auctor in eu erat. Fusce eu lacinia metus, id ultricies leo. Integer tempus, ante at mattis suscipit, arcu dolor venenatis mauris, molestie consequat erat mi. </p>
+            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque a augue magna. Maecenas vitae tellus sed ligula viverra pulvinar. Donec in justo sagittis magna egestas auctor in eu erat. Fusce eu lacinia metus, id ultricies leo. Integer tempus, ante at mattis suscipit, arcu dolor venenatis mauris, molestie consequat erat mi. </p>
+          </div>
+        </div>
       </div>
     </div>
   )
--- src/TopBar.js
+++ src/TopBar.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import Logo from "./logo.svg"
+import {useNavigate} from "react-router-dom"
 
 export default function TopBar() {
+  let navigate = useNavigate();
   return (
     <div className='navBar slim'>
-        <button style={{"flex":"none"}} className='textButton'><img style={{height:"100%"}} src={Logo}/></button>
-        <button>
-            jslkf
-        </button>
+        <button onClick={()=>{navigate("/")}} style={{"flex":"none"}} className='textButton'><img style={{height:"100%"}} src={Logo}/></button>
     </div>
   )
 }
