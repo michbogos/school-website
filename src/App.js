9634cac73038657e5e91b13f22f6198c413731f7
Michael Bogosta <bogostamicheal@gmail.com>
2022-11-19 16:03:15 +0100

    Clean up rest of the files.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
 import './App.css';
 import { Routes, Route, Link } from "react-router-dom";
 import Homepage from './Homepage';
-import Food from './Food';
-
-import Wir from './Wir'
-import Schule from './Schule'
-import Vario from './Vario'
-import Org from './Org'
-import Kontakt from './Kontakt'
-import Matura from './Matura';
 
 function App() {
   return (
     <Routes>
       <Route path='/school-website' element={<Homepage/>}></Route>
-      <Route path='/recepies' element={<Food/>}></Route>
-      <Route path='/wir' element={<Wir></Wir>}></Route>
-      <Route path='/matura' element={<Matura></Matura>}></Route>
-      <Route path='/vario' element={<Vario></Vario>}></Route>
-      <Route path='/zweige' element={<Org></Org>}></Route>
-      <Route path='/kontakt' element={<Kontakt></Kontakt>}></Route>
-      <Route path='/arbeitsauftrag' element={<Food></Food>}></Route>
     </Routes>
   );
 }
