d2a6e4335e8aee648582642c445a87fb0e9cdbb6
Michael Bogosta <bogostamicheal@gmail.com>
2023-01-27 21:55:41 +0100

    Add content

--- src/pages/Schule.js
+++ src/pages/Schule.js
@@ -1,16 +1,21 @@
+import { Typography, Stack, Table, TableRow, TableCell, TableHead, TableBody, TableFooter } from '@mui/material'
+import { Container } from '@mui/system'
 import React from 'react'
-
-import TopBar from '../components/TopBar'
-import {Box, Typography} from "@mui/material"
+import Utable from '../components/Utable'
 
 export default function Schule() {
   return (
-    <div>
-      <TopBar></TopBar>
-      <Box>
-        <img style={{height:"100vh", width:"100vw"}} src="https://upload.wikimedia.org/wikipedia/commons/8/82/Schloss_Traunsee.JPG"></img>
-      </Box>
-      <Typography variant="h1">Unsere Schule</Typography>
-    </div>
+    <Stack direction="row">
+      <div>
+        <Typography textAlign="center" variant="h1">Unterstufe</Typography>
+        <Container maxWidth="md">
+          <Typography variant="body1">In der Unterstufe wird das wirtschaftskundliche Realgymnasium und das naturwissenschaftliche Realgymnasium gemeinsam unterrichtet. Die Typenwahl ist erst <strong>ab der 5. Klasse</strong> erforderlich.</Typography>
+          <Utable></Utable>
+        </Container>
+      </div>
+      <div>
+        
+      </div>
+    </Stack>
   )
 }
